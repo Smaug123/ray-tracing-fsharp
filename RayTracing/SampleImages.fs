c0da461a26631d1db249ae7f1d82af65f22129b4
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2021-04-03 11:48:17 +0100

    Chapter 2 done

--- RayTracing/SampleImages.fs
+++ RayTracing/SampleImages.fs
@@ -4,17 +4,17 @@ namespace RayTracing
 module SampleImages =
 
     let gradient (progressIncrement : float<progress> -> unit) : float<progress> * Image Async =
-        let pixelAt i j =
+        let pixelAt height width =
             {
-                Red = (byte i)
-                Green = (byte j)
-                Blue = 64uy
+                Red = (byte width)
+                Green = 255uy - (byte height)
+                Blue = 63uy
             }
 
         256.0<progress>,
         async {
-            return Array.init 256 (fun i ->
-                let output = Array.init 256 (pixelAt i)
+            return Array.init 256 (fun height ->
+                let output = Array.init 256 (pixelAt height)
                 progressIncrement 1.0<progress>
                 output
             )
